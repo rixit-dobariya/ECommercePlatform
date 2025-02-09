9e63715691dc4b7c008a9329aaf45d7b9dc3fc61
Rixit Dobariya <Tpxitachi05@outlook.com>
2025-02-09 23:37:55 +0530

    Complete wishlist CRUD operations

--- ECommercePlatform/Areas/Customer/Controllers/WishlistController.cs
+++ ECommercePlatform/Areas/Customer/Controllers/WishlistController.cs
@@ -1,38 +1,77 @@
-﻿using ECommercePlatform.Models;
+﻿using ECommercePlatform.Constants;
+using ECommercePlatform.Models;
 using ECommercePlatform.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommercePlatform.Areas.Customer.Controllers
 {
+    [Area(UserRole.Customer)]
     public class WishlistController : Controller
     {
-        IUnitOfWork _unitOfWork { get; set; }
+        IUnitOfWork _unitOfWork;
         public WishlistController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
-        public IActionResult Index()
+        public IActionResult Index(IEnumerable<WishlistItem> wishlistItems)
         {
-            return View();
-        }
-        public IActionResult Add(int productId)
-        {
-            if (productId == null || productId == 0)
-            {
-                return RedirectToActionPermanent("Index");
-            }
-            string userId = HttpContext.Session.GetString("UserId");
+            string? userId = HttpContext.Session.GetString("UserId");
             if (userId == null)
             {
                 TempData["error"] = "You must be logged in to access this page";
                 return RedirectToAction("Login", "User");
             }
+            wishlistItems = _unitOfWork.WishlistItems.GetAll("Product")
+                .Where(ci => ci.UserId == Convert.ToInt32(userId));
+            return View(wishlistItems);
+        }
+        public IActionResult Add(int productId, int quantity = 1)
+        {
+            if (productId == 0)
+            {
+                return RedirectToActionPermanent("Index");
+            }
+            string? userId = HttpContext.Session.GetString("UserId");
+            if (userId == null)
+            {
+                TempData["error"] = "You must be logged in to access this page";
+                return RedirectToAction("Login", "User");
+            }
+            WishlistItem checkWishlistItem = _unitOfWork.WishlistItems.Get(ci => productId == ci.ProductId && ci.UserId == Convert.ToInt32(userId));
+            if (checkWishlistItem != null)
+            {
+                TempData["error"] = "Product is already available in wishlist!";
+                return RedirectToActionPermanent("Index");
+            }
+            //add product to wishlist
             WishlistItem wishlistItem = new()
             {
                 ProductId = productId,
                 UserId = Convert.ToInt32(userId)
             };
             _unitOfWork.WishlistItems.Add(wishlistItem);
+            _unitOfWork.Save();
+            TempData["success"] = "Product added to wishlist successfully!";
+            return RedirectToActionPermanent("Index");
+        }
+        public IActionResult Remove(int productId)
+        {
+            string? userId = HttpContext.Session.GetString("UserId");
+            if (productId == 0)
+            {
+                TempData["error"] = "Deletion failed";
+            }
+            else if (userId == null)
+            {
+                TempData["error"] = "Deletion failed";
+            }
+            else
+            {
+                WishlistItem wishlistItem = _unitOfWork.WishlistItems.Get(ci => ci.ProductId == productId && ci.UserId == Convert.ToInt32(userId));
+                _unitOfWork.WishlistItems.Remove(wishlistItem);
+                _unitOfWork.Save();
+                TempData["success"] = "Product removed from wishlist successfully!";
+            }
             return RedirectToActionPermanent("Index");
         }
     }
--- ECommercePlatform/Models/WishlistItem.cs
+++ ECommercePlatform/Models/WishlistItem.cs
@@ -1,14 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ECommercePlatform.Models
 {
     public class WishlistItem
     {
         public int ProductId { get; set; }
+        [ValidateNever]
         [ForeignKey("ProductId")]
         public Product Product { get; set; }    
 
         public int UserId { get; set; }
+        [ValidateNever]
         [ForeignKey("UserId")]
         public User User { get; set; }
     }
