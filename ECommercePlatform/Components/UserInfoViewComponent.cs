c5dacf411229ef60641e98eb1c059c6a5ab8cb2e
Rixit Dobariya <Tpxitachi05@outlook.com>
2025-02-11 01:20:10 +0530

    Do some changes in header in user side

---
+++ ECommercePlatform/Components/UserInfoViewComponent.cs
@@ -0,0 +1,49 @@
+﻿using ECommercePlatform.Constants;
+using ECommercePlatform.Models;
+using ECommercePlatform.Models.ViewModels;
+using ECommercePlatform.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommercePlatform.Components
+{
+    [Area(UserRole.Customer)]
+    public class UserInfoViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserInfoViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            string? userId = HttpContext.Session.GetString("UserId");
+            UserInfoVM userInfoVM = new();
+            int shippingCharge = 50;
+
+            if (!string.IsNullOrEmpty(userId))
+            {
+                int userIdInt = Convert.ToInt32(userId);
+
+                userInfoVM.CartItems = _unitOfWork.CartItems.GetAll("Product")
+                    .Where(ci => ci.UserId == userIdInt) ?? new List<CartItem>();
+
+                userInfoVM.WishlistCount = _unitOfWork.WishlistItems.GetAll()
+                    ?.Count(wi => wi.UserId == userIdInt) ?? 0;
+
+                userInfoVM.ShippingCharge = shippingCharge;
+
+                userInfoVM.Total = userInfoVM.CartItems
+                    .Select(ci => (ci.Product.SellPrice - (ci.Product.SellPrice * ci.Product.Discount / 100)) * ci.Quantity)
+                    .DefaultIfEmpty(0)
+                    .Sum() + shippingCharge;
+
+                userInfoVM.ProfilePictureUrl = _unitOfWork.Users.Get(u => u.UserId == userIdInt)?.ProfilePicture
+                    ??"";
+            }
+
+            return View(userInfoVM);
+        }
+    }
+}
---
+++ ECommercePlatform/Models/ViewModels/UserInfoVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommercePlatform.Models.ViewModels
+{
+    //to display some user info in layout file
+    public class UserInfoVM
+    {
+        public int WishlistCount { get; set; }
+        public string ProfilePictureUrl { get;set; }
+        public IEnumerable<CartItem> CartItems { get; set; }
+        public decimal ShippingCharge { get; set; } = 0;
+        public decimal Total { get; set; }
+    }
+}
