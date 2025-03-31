322a7a30404ebe1c1e612db66c59bd066b34ac65
Rixit Dobariya <rixitdobariya05@gmail.com>
2025-03-31 16:29:50 +0530

    Made necesaary changes

--- ECommercePlatform/Areas/Admin/Controllers/HomeController.cs
+++ ECommercePlatform/Areas/Admin/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using ECommercePlatform.Constants;
 using ECommercePlatform.Filters;
+using ECommercePlatform.Models;
+using ECommercePlatform.Repository;
+using ECommercePlatform.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommercePlatform.Areas.Admin.Controllers
 {
@@ -8,13 +12,26 @@ namespace ECommercePlatform.Areas.Admin.Controllers
     [AdminAuthCheck]
     public class HomeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             ViewData["ActiveMenu"] = "Home";
 
-            return View();
+            DashboardVM dashboardVM = new DashboardVM
+            {
+                TotalUsers = await _unitOfWork.Users.GetAll().CountAsync(),
+                TotalProducts = await _unitOfWork.Products.GetAll().CountAsync(),
+                TotalSales = await _unitOfWork.OrderDetails.GetAll().SumAsync(o => o.Price * o.Quantity),
+                TotalOrders = await _unitOfWork.OrderHeaders.GetAll().CountAsync()
+            };
+
+            return View(dashboardVM);
         }
-        
+
     }
 }
---
+++ ECommercePlatform/Models/DashboardVM.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommercePlatform.Models
+{
+    public class DashboardVM
+    {
+        public int TotalUsers { get; set; }
+        public int TotalProducts { get; set; }
+        public decimal TotalSales { get; set; }
+        public int TotalOrders { get; set; }
+    }
+
+}
